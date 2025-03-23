69ab3511c492d62b3b386e591800867575a54700
speedy <thenewamirech@gmail.com>
2025-03-24 02:26:33 +0330

    added Taco

---
+++ src/main/java/ir/jiring/tacocloud/domain/Taco.java
@@ -0,0 +1,13 @@
+package ir.jiring.tacocloud.domain;
+
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+
+    private List<Ingredient> ingredients;
+}
